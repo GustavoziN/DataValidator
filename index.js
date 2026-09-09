4515b55a097953db41c9d761cc328b838f7c16f8
GustavoZin <slepziin@gmail.com>
2026-09-09 18:29:55 -0300

    DataValidator1.0

--- index.js
+++ index.js
@@ -20,7 +20,44 @@ function MostrarNoConsole() {
     console.log('Nenhum arquivo foi convertido ainda.');
     return;
   }
-  console.log(JSON.stringify(dadosConvertidos, null, 2));
-}
 
+  // remove a linha fantasma (o cabeçalho que "vazou" para dentro dos dados)
+  const dados = dadosConvertidos.filter(function (linha) {
+    return linha['__EMPTY'] !== 'TP Movimento';
+  });
+
+  // agrupa por produto, somando quantidade produzida (010) e movimentada (999)
+  const porProduto = {};
+
+  dados.forEach(function (linha) {
+    const produto = linha['__EMPTY_1'];
+    const tipo = linha['__EMPTY'];
+    const quantidade = Number(linha['__EMPTY_3']) || 0;
+
+    if (!porProduto[produto]) {
+      porProduto[produto] = { produzido: 0, movimentado: 0 };
+    }
+
+    if (tipo === '010') {
+      porProduto[produto].produzido += quantidade;
+    } else if (tipo === '999') {
+      porProduto[produto].movimentado += quantidade;
+    }
+  });
+
+  // monta o relatório final
+  const relatorio = Object.keys(porProduto).map(function (produto) {
+    const item = porProduto[produto];
+    const bate = item.produzido === item.movimentado;
+    return {
+      Produto: produto,
+      Produzido: item.produzido,
+      Movimentado: item.movimentado,
+      Diferenca: item.produzido - item.movimentado,
+      Bate: bate ? 'SIM' : 'NAO'
+    };
+  });
+
+  console.log(JSON.stringify(relatorio, null, 2));
+}
 
